ac8cfb13b9be5ca836d2a404c2ef55622c90f024
Andrefer-Santtos <1andre.santos.souza1@gmail.com>
2021-09-17 23:46:26 -0300

    Boiler Plate

---
+++ src/game/Jogo.java
@@ -0,0 +1,44 @@
+package game;
+
+import java.awt.Container;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Jogo extends JFrame {
+
+	private JPanel pane;
+	private Container container;
+
+	public Jogo() {
+
+		inicializarComponentes();
+		definirEventos();
+
+	}
+
+	private void inicializarComponentes() {
+
+		container = getContentPane();
+
+		pane = new JPanel(null);
+
+	}
+
+	private void definirEventos() {
+
+	}
+
+	public static void main(String args[]) {
+
+		Jogo frame = new Jogo();
+		frame.setTitle("Hand-Gun");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1024, 768);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setVisible(true);
+
+	}
+
+}
